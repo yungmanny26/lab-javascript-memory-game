824152bd74ea6d01914716585905b8049c630354
yungmanny26 <tondreau24@gmail.com>
2022-02-12 09:11:17 -0500

    solved lab tondreau

--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,10 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards =[];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
     // add the rest of the class properties here
   }
 
@@ -10,9 +14,22 @@ class MemoryGame {
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true
+    }else {
+      return false;
+    }
   }
 
   checkIfFinished() {
+    let answer = this.pairsGuessed === (this.cards.length / 2);
+    if (answer) {
+      return true;
+    } else {
+      return false;
+    }
     // ... write your code here
   }
 }
